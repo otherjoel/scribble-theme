d4bdc938c1be68af998909ea45da4b047cf76ee9
Joel Dueck <joel@jdueck.net>
2025-11-23 12:21:34 -0600

    Update deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 (define collection "scribble-theme")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("scribble-lib"
+               "base"))
+(define build-deps '("scribble-doc"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/scribble-theme.scrbl" () ("Scribble Libraries"))))
 (define pkg-desc "Replace scribble/manual CSS styles with custom ones")
 (define version "2.0")
